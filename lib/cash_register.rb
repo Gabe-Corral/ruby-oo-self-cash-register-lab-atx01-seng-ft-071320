b758b7c175eca0133f74bb7608ae9a7eacd31431
Joseph Corral <78gabe.corral@gmail.com>
2020-07-16 16:56:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
-
+class CashRegister
+  attr_accessor :discount
+  attr_accessor :total
+  attr_accessor :item
+  @@array = []
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity=1)
+    @quantity = quantity
+    @title = title
+    @total += price * quantity
+    @total
+  end
+  
+  def apply_discount
+    @total -= @discount
+    "After the discount, the total comes to $#{@total}."
+  end
+  
+  def items
+    @@array
+  end
+end
